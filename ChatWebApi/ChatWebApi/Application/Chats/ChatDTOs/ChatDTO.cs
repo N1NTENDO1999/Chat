41befdece422839b246411fe5fa1a524636e5c72
Denys Nazarko <43789735+N1NTENDO1999@users.noreply.github.com>
2020-02-12 11:58:56 +0200

    Add ChatDto

---
+++ ChatWebApi/ChatWebApi/Application/Chats/ChatDTOs/ChatDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ChatWebApi.Application.Users.Queries.UserDTOs;
+
+namespace ChatWebApi.Application.Chats.ChatDTOs
+{
+	public class ChatDTO
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Picture { get; set; }
+		public DateTime DateCreated { get; set; }
+		public bool IsPrivate { get; set; }
+
+		public List<UserDTO> Users { get; set; }
+	}
+}
