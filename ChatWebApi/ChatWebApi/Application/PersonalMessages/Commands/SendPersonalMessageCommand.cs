a4dcc2f1253f610e1e12f5d45e609ebc7cee1d15
Denys Nazarko <43789735+N1NTENDO1999@users.noreply.github.com>
2020-02-13 17:06:23 +0200

    PersonalMessagesCQ

---
+++ ChatWebApi/ChatWebApi/Application/PersonalMessages/Commands/SendPersonalMessageCommand.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using ChatWebApi.Infrastructure;
+using ChatWebApi.Infrastructure.Entities;
+using ChatWebApi.Interfaces.Requests;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChatWebApi.Application.PersonalMessages.Commands
+{
+	public class SendPersonalMessageCommand : IRequest<CommandResult>
+	{
+		public int ReceiverId { get; set; }
+		public int SenderId { get; set; }
+		public string Text { get; set; }
+	}
+
+	public class SendPersonalMessageCommandHandler : IRequestHandler<SendPersonalMessageCommand, CommandResult>
+	{
+		private readonly ChatContext _db;
+
+		public SendPersonalMessageCommandHandler(ChatContext chatContext)
+		{
+			_db = chatContext;
+		}
+
+		public async Task<CommandResult> Handle(SendPersonalMessageCommand request, CancellationToken cancellationToken)
+		{
+			if (string.IsNullOrWhiteSpace(request.Text))
+				throw new ArgumentNullException("No Text In Message", nameof(request));
+
+			var receiver = await _db.Users.FirstAsync(p => p.Id == request.ReceiverId);
+			var sender = await _db.Users.FirstAsync(p => p.Id == request.SenderId);
+
+			var message = new PersonalMessage { Receiver = receiver, Sender = sender, Text = request.Text, DateCreated = DateTime.UtcNow };
+			await _db.PersonalMessages.AddAsync(message);
+			await _db.SaveChangesAsync();
+
+			return new CommandResult();
+		}
+	}
+}
---
+++ ChatWebApi/ChatWebApi/Application/PersonalMessages/Queries/GetUserPersonalMessagesQuery.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using ChatWebApi.Application.PersonalMessages.PersonalMessageDTOs;
+using ChatWebApi.Infrastructure;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChatWebApi.Application.PersonalMessages.Queries
+{
+	public class GetUserPersonalMessagesQuery : IRequest<GetUserPersonalMessagesQueryResult>
+	{
+		public int SenderId { get; set; }
+		public int ReceiverId { get; set; }
+	}
+
+	public class GetUserPersonalMessagesQueryResult
+	{
+		public List<PersonalMessageDTO> Messages { get; set; }
+	}
+
+	public class GetUserPersonalMessagesQueryHandler : IRequestHandler<GetUserPersonalMessagesQuery, GetUserPersonalMessagesQueryResult>
+	{
+		private readonly ChatContext _db;
+		private readonly IMapper _mapper;
+
+		public GetUserPersonalMessagesQueryHandler(ChatContext chatContext, IMapper mapper)
+		{
+			_db = chatContext;
+			_mapper = mapper;
+		}
+
+		public async Task<GetUserPersonalMessagesQueryResult> Handle(GetUserPersonalMessagesQuery request, CancellationToken cancellationToken)
+		{
+			var messages = await _db.PersonalMessages.Include(p => p.Receiver).Include(p => p.Sender)
+				.Where(p => (p.SenderId == request.SenderId & p.ReceiverId == request.ReceiverId) |
+						(p.SenderId == request.ReceiverId & p.ReceiverId == request.SenderId))
+				.ToListAsync();
+
+			var result = _mapper.Map<List<PersonalMessageDTO>>(messages);
+
+			return new GetUserPersonalMessagesQueryResult { Messages = result };
+
+		}
+	}
+}
