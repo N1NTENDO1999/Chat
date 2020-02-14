3ee2b22420c1075b8935a4c970ea2a8cc3a82520
Denys Nazarko <43789735+N1NTENDO1999@users.noreply.github.com>
2020-02-14 13:57:55 +0200

    Get user activity status

---
+++ ChatWebApi/ChatWebApi/Application/PersonalMessages/Queries/GetUserActivityStatusQuery.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using ChatWebApi.Infrastructure;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChatWebApi.Application.PersonalMessages.Queries
+{
+	public class GetUserActivityStatusQuery : IRequest<GetUserActivityStatusQueryResult>
+	{
+		public int UserId { get; set; }
+	}
+
+	public class GetUserActivityStatusQueryResult
+	{
+		public DateTime ActiveTime { get; set; }
+	}
+
+	public class GetUserActivityStatusQueryHandler : IRequestHandler<GetUserActivityStatusQuery, GetUserActivityStatusQueryResult>
+	{
+		private readonly ChatContext _db;
+
+		public GetUserActivityStatusQueryHandler(ChatContext chatContext)
+		{
+			_db = chatContext;
+		}
+
+		public async Task<GetUserActivityStatusQueryResult> Handle(GetUserActivityStatusQuery request, CancellationToken cancellationToken)
+		{
+			var user = await _db.Users.FirstAsync(p => p.Id == request.UserId);
+
+			return new GetUserActivityStatusQueryResult { ActiveTime = user.ActiveDateTime };
+		}
+	}
+}
