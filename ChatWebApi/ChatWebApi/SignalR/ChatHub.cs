7412f574572e55c0bcdb1a7fc0a398cec496f7d3
Denys Nazarko <43789735+N1NTENDO1999@users.noreply.github.com>
2020-02-14 16:16:10 +0200

    Add few base methods in hub

--- ChatWebApi/ChatWebApi/SignalR/ChatHub.cs
+++ ChatWebApi/ChatWebApi/SignalR/ChatHub.cs
@@ -2,10 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ChatWebApi.Application.Messages.Commands;
+using ChatWebApi.Application.Messages.Queries;
+using MediatR;
+using Microsoft.AspNetCore.SignalR;
 
 namespace ChatWebApi.SignalR
 {
-	public class ChatHub
+	public class ChatHub : Hub
 	{
+		private readonly IMediator _mediator;
+
+		public ChatHub(IMediator mediator)
+		{
+			_mediator = mediator;
+		}
+
+		public async Task SendMessageToChat(int userId, int chatId, string message)
+		{
+			await _mediator.Send(new SendMessageCommand { ChatId = chatId, SenderId = userId, Text = message });
+			await Clients.All.SendAsync("UpdateChatMessages", chatId, userId, message);
+		}
+
+		public async Task GetChatMessages(int chatId)
+		{
+			var result = await _mediator.Send(new GetChatMessagesQuery { ChatId = chatId });
+			await Clients.All.SendAsync("GetChatMessages", chatId, result.Messages);
+		}
 	}
 }
