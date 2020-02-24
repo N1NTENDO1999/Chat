e9d203fc095de5c6e7b5203f5a2fc006e0dfc8f0
Denys Nazarko <43789735+N1NTENDO1999@users.noreply.github.com>
2020-02-24 17:53:10 +0200

    fix names

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ChatService } from './core/http/Chat.service';
+import { BaseApiSettingsService } from './settings/BaseApiSettings.service';
+import { ChatsApiSettingsService } from './settings/ChatsApiSettings.service';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,10 @@ import { ChatService } from './core/http/Chat.service';
     AppRoutingModule
   ],
   providers: [
-    ChatService
+    ChatService,
+    BaseApiSettingsService,
+    ChatsApiSettingsService
+
   ],
   bootstrap: [AppComponent]
 })
--- ClientApp/src/app/core/models/Chat.ts
+++ ClientApp/src/app/core/models/Chat.ts
@@ -1,4 +1,4 @@
-export class Hero {
+export class Chat {
     id: number;
     name: string;
     picture: string;
