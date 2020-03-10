bc26624cb7012997deb8b8ed862f94758aad2355
Denys Nazarko <43789735+N1NTENDO1999@users.noreply.github.com>
2020-03-10 13:36:36 +0200

    Change Login View

--- ClientApp/src/app/shared/components/login.component.ts
+++ ClientApp/src/app/shared/components/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 import { first } from 'rxjs/operators';
 import { AuthenticationService } from '../../core/services/Authentication.service';
 import { AlertService } from '../../core/services/Alert.service';
@@ -33,7 +33,9 @@ export class LoginComponent implements OnInit {
 
     ngOnInit() {
         this.loginForm = this.formBuilder.group({
-            email: ['', Validators.email],
+            email: new FormControl('',[
+                Validators.required,
+                Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$")]),
             password: ['', Validators.required]
         });
 
