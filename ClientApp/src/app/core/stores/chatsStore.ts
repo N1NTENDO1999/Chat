9c7c53b7912b36faa386b071caf9c5e164af2bc3
Denys Nazarko <43789735+N1NTENDO1999@users.noreply.github.com>
2020-03-19 15:04:56 +0200

    set base methods

--- ClientApp/src/app/core/stores/chatsStore.ts
+++ ClientApp/src/app/core/stores/chatsStore.ts
@@ -11,8 +11,28 @@ export class ChatsStore {
         return this.selectedChat;
     }
 
-    @action setChats(chats: ChatDto[]){
+    @computed get allChats(){
+        return this.chats;
+    }
+
+    @computed get selectedChatId(){
+        return this.selectedChat.Id;
+    }
+
+    @action setChats(chats: ChatDto[]) {
         console.log(chats);
         this.chats = chats;
+    }
+
+    @action selectChat(chat: ChatDto) {
+        this.selectedChat = chat;
+    }
+
+    @action clearSelected(){
+        this.selectedChat = null;
+    }
+
+    @action clearchats(){
+        this.chats = [];
     }
 } 
