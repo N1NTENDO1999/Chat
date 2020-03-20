429e035c918e3a9ba426f28785ad3de13e930cc5
Denys Nazarko <43789735+N1NTENDO1999@users.noreply.github.com>
2020-03-20 13:32:26 +0200

    Add messages Store

--- ClientApp/src/app/core/stores/MessagesStore.ts
+++ ClientApp/src/app/core/stores/MessagesStore.ts
@@ -5,4 +5,12 @@ import { MessageDto } from '../models/MessageDto';
 @Injectable()
 export class MessagesStore{
     @observable messages: MessageDto[] = [];
+
+    @computed get allMessages(): MessageDto[]{
+        return toJS(this.messages);
+    }
+
+    set message(value: MessageDto){
+        this.messages = [...this.messages, value];
+    }
 }
