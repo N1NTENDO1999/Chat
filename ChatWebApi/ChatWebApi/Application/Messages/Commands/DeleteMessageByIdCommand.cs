fc7fa9176c9e40562fcf5dd0d74bfefcc43cfae0
Denys Nazarko <43789735+N1NTENDO1999@users.noreply.github.com>
2020-03-25 15:49:46 +0200

    Add async hosted service to send scheduled messages

---
+++ ChatWebApi/ChatWebApi/Application/Messages/Commands/DeleteMessageByIdCommand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using ChatWebApi.Infrastructure;
+using ChatWebApi.Interfaces.Requests;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChatWebApi.Application.Messages.Commands
+{
+	public class DeleteMessageByIdCommand : IRequest<CommandResult>
+	{
+		public int Id { get; set; }
+	}
+
+	public class DeleteMessageByIdCommandHandler : IRequestHandler<DeleteMessageByIdCommand, CommandResult>
+	{
+		private readonly ChatContext _db;
+
+		public DeleteMessageByIdCommandHandler(ChatContext chatContext)
+		{
+			_db = chatContext;
+		}
+
+		public async Task<CommandResult> Handle(DeleteMessageByIdCommand request, CancellationToken cancellationToken)
+		{
+			var message = await _db.Messages.FirstAsync(p => p.Id == request.Id);
+			message.Text = "Updated";
+			_db.Messages.Update(message);
+			await _db.SaveChangesAsync();
+
+			return new CommandResult();
+		}
+	}
+}
--- ChatWebApi/ChatWebApi/Application/Services/ScheduledMessagesSender.cs
+++ ChatWebApi/ChatWebApi/Application/Services/ScheduledMessagesSender.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using ChatWebApi.Application.Messages.Commands;
+using ChatWebApi.Application.Messages.MessageDTOs;
+using ChatWebApi.Application.Messages.Queries;
+using ChatWebApi.Application.PersonalMessages.Commands;
+using ChatWebApi.Application.PersonalMessages.Queries;
+using ChatWebApi.Application.ScheduledMessages.Commands;
 using ChatWebApi.Application.ScheduledMessages.Queries;
 using ChatWebApi.Infrastructure;
 using ChatWebApi.Interfaces;
@@ -17,7 +23,6 @@ namespace ChatWebApi.Application.Services
 	{
 		private readonly IMediator _mediator;
 		private readonly IHubContext<ChatHub> _hub;
-		private int executionCount = 0;
 		public ScheduledMessagesSender(IMediator mediator, IHubContext<ChatHub> hubContext)
 		{
 			_mediator = mediator;
@@ -28,13 +33,58 @@ namespace ChatWebApi.Application.Services
 		{
 			while (!stoppingToken.IsCancellationRequested)
 			{
-				executionCount++;
 				var message = await _mediator.Send(new GetTopByDeliverMessageQuery());
+				if (message == null)
+				{
+					await _hub.Clients.All.SendAsync("Msq", "No More Scheduled Chats");
+					await Task.Delay(10000, stoppingToken);
+					continue;
+				}
+				MessageDTO executedMessage = null;
+				if (message != null & message.Delivery <= DateTime.Now)
+				{
+					if (message.IsPersonal)
+					{
+						var resultId = await _mediator
+							.Send(new SendPersonalMessageCommand
+							{ ReceiverId = message.ReceiverId, SenderId = message.SenderId, Text = message.Text }
+							);
 
-				await _hub.Clients.All.SendAsync("Msq", message);
-				
+						var resultMessage = await _mediator
+							.Send(new GetPersonalMessageByIdQuery { Id = resultId.Id });
 
-				await Task.Delay(10000, stoppingToken);
+						await _mediator.Send(new DeleteScheduledMessageByIdCommand { Id = message.Id });
+
+						executedMessage = resultMessage.Message;
+
+						var min = Math.Min(message.ReceiverId, message.SenderId).ToString();
+						var max = Math.Max(message.ReceiverId, message.SenderId).ToString();
+
+						await _hub.Clients.Group(min + "+" + max)
+							.SendAsync("UpdateChatMessages", executedMessage, message.ReceiverId, true);
+					}
+					else
+					{
+						var resultId = await _mediator
+							.Send(new SendMessageCommand
+							{ ChatId = message.ReceiverId, SenderId = message.SenderId, Text = message.Text }
+							);
+
+						var resultMessage = await _mediator
+							.Send(new GetChatMessageByIdQuery { Id = resultId.Id });
+
+						await _mediator.Send(new DeleteScheduledMessageByIdCommand { Id = message.Id });
+
+						executedMessage = resultMessage.Message;
+
+						await _hub.Clients.Group(message.ReceiverId.ToString())
+							.SendAsync("UpdateChatMessages", executedMessage, message.ReceiverId, false);
+					}
+				}
+				else
+				{
+					await Task.Delay(10000, stoppingToken);
+				}
 			}
 		}
 	}
