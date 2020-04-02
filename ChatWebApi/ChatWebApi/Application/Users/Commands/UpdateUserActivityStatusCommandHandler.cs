408e67f955d88ae4748ba86e3d47892143da0cff
Denys Nazarko <43789735+N1NTENDO1999@users.noreply.github.com>
2020-04-02 16:51:32 +0300

    add Update user activity status

---
+++ ChatWebApi/ChatWebApi/Application/Users/Commands/UpdateUserActivityStatusCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using ChatWebApi.Infrastructure;
+using ChatWebApi.Interfaces.Requests;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChatWebApi.Application.Users.Commands
+{
+	public class UpdateUserActivityStatusCommand : IRequest<CommandResult>
+	{
+		public int UserId { get; set; }
+	}
+
+	public class UpdateUserActivityStatusCommandHandler : IRequestHandler<UpdateUserActivityStatusCommand, CommandResult>
+	{
+		private readonly ChatContext _db;
+
+		public UpdateUserActivityStatusCommandHandler(ChatContext chatContext)
+		{
+			_db = chatContext;
+		}
+
+		public async Task<CommandResult> Handle(UpdateUserActivityStatusCommand request, CancellationToken cancellationToken)
+		{
+			var user = await _db.Users.FirstAsync(p => p.Id == request.UserId);
+			user.ActiveDateTime = DateTime.Now;
+			await _db.SaveChangesAsync();
+			return new CommandResult();
+		}
+	}
+}
