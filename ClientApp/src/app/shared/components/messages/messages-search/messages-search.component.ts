957b0d73677f9f18097957da7d80dc582ca474be
Denys Nazarko <43789735+N1NTENDO1999@users.noreply.github.com>
2020-04-06 13:52:40 +0300

    setup search store

--- ClientApp/src/app/shared/components/messages/messages-search/messages-search.component.ts
+++ ClientApp/src/app/shared/components/messages/messages-search/messages-search.component.ts
@@ -1,17 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { SignalrService } from 'src/app/core/signalR/SignalR.service';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
     selector: 'messages-search-component',
     templateUrl: './messages-search.component.html',
     styleUrls: ['./messages-search.component.css']
 })
-export class MessagesSearchComponent implements OnInit {
+export class MessagesSearchComponent implements OnInit, OnDestroy {
+
+    private _searchSubject: Subject<string> = new Subject();
 
     constructor(
         private signalRService: SignalrService,
 
     ) {
+        this._setSearchSubscription();
+    }
+    ngOnDestroy(): void {
+        this._searchSubject.unsubscribe();
+    }
+
+    private _setSearchSubscription() {
+        this._searchSubject.pipe(
+            debounceTime(500)
+        ).subscribe((term: string) => {
+            if (term.trim()) {
+                console.log(term);
+            }
+        });
+    }
+
+    public updateSearch(searchTextValue: string) {
+        this._searchSubject.next(searchTextValue);
     }
 
     ngOnInit() {
@@ -20,7 +42,7 @@ export class MessagesSearchComponent implements OnInit {
 
     search(term: string): void {
         if (term.trim()) {
-           console.log(term);
+            console.log(term);
         }
     }
 
