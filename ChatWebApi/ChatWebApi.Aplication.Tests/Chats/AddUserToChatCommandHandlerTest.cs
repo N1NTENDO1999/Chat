1cfd6f7e278de9e2da70c20792628347491815a5
Denys Nazarko <43789735+N1NTENDO1999@users.noreply.github.com>
2020-04-24 11:20:24 +0300

    add two tests for add user to chat command handler

--- ChatWebApi/ChatWebApi.Aplication.Tests/Chats/AddUserToChatCommandHandlerTest.cs
+++ ChatWebApi/ChatWebApi.Aplication.Tests/Chats/AddUserToChatCommandHandlerTest.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using AutoMapper;
+using ChatWebApi.Application.Chats.ChatDTOs;
 using ChatWebApi.Application.UserChats.Commands;
 using ChatWebApi.Infrastructure;
 using ChatWebApi.Infrastructure.Entities;
@@ -21,12 +22,13 @@ namespace ChatWebApi.Aplication.Tests.Chats
 		public async void When_User_Is_Null_Then_Throws_Error()
 		{
 			var options = new DbContextOptionsBuilder<ChatContext>()
-				.UseInMemoryDatabase(databaseName: "MovieListDatabase")
+				.UseInMemoryDatabase(databaseName: "ChatDatabase")
 				.Options;
-			var command = new AddUserToChatCommand { ChatId = 1, UserId = 1 };
+			var command = new AddUserToChatCommand { ChatId = 2, UserId = 2 };
 			var mapper = new Mock<IMapper>();
+			mapper.Setup(p => p.Map<ChatDTO>(It.IsAny<Chat>()));
 
-			using (var context = new ChatContext(options)) 
+			using (var context = new ChatContext(options))
 			{
 				await context.Users.AddAsync(new User { Id = 1 });
 				await context.Chats.AddAsync(new Chat { Id = 1 });
@@ -37,7 +39,34 @@ namespace ChatWebApi.Aplication.Tests.Chats
 			{
 				var handler = new AddUserToChatCommandHandler(context, mapper.Object);
 
-				await Assert.ThrowsAsync<ArgumentNullException>(() => handler.Handle(command, It.IsAny<CancellationToken>()));
+				await Assert.ThrowsAsync<InvalidOperationException>(() => handler.Handle(command, It.IsAny<CancellationToken>()));
+			}
+
+		}
+
+		[Fact]
+		public async void When_User_Not_Null_Then_Returns_User()
+		{
+			var options = new DbContextOptionsBuilder<ChatContext>()
+				.UseInMemoryDatabase(databaseName: "ChatDatabase")
+				.Options;
+			var command = new AddUserToChatCommand { ChatId = 2, UserId = 2 };
+			var mapper = new Mock<IMapper>();
+			mapper.Setup(p => p.Map<ChatDTO>(It.IsAny<Chat>())).Returns(new ChatDTO { Id = 2});
+
+			using (var context = new ChatContext(options))
+			{
+				await context.Users.AddAsync(new User { Id = 2 });
+				await context.Chats.AddAsync(new Chat { Id = 2 });
+				await context.SaveChangesAsync();
+			}
+
+			using (var context = new ChatContext(options))
+			{
+				var handler = new AddUserToChatCommandHandler(context, mapper.Object);
+				var result = await handler.Handle(command, It.IsAny<CancellationToken>());
+
+				Assert.Equal(2, result.Id);
 			}
 
 		}
